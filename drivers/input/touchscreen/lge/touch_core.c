a690a866c5a63d4b667ceae78202f54b51eb04e8
Elliott Mitchell <ehem+android_kernel_lge_msm8996@m5p.com>
2017-03-12 12:50:19 -0700

    Fix disabling of second screen
    The original change, a2ed7a25e81eeeb8ebe42d281ace647b8439204b was done
    very poorly.  This is needed for LineageOS currently, but work is well
    under way to fix that.  Other Android versions may need these in either
    setup, so make it easy to choose.  By turning this into a configuration
    option, it can be enabled/disabled without needing to modify the source
    code.

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -396,6 +396,13 @@ static int touch_init_input(struct touch_core_data *ts)
 	set_bit(INPUT_PROP_DIRECT, input->propbit);
 	input_set_abs_params(input, ABS_MT_POSITION_X, 0,
 			ts->caps.max_x, 0, 0);
+<<<<<<< HEAD
+=======
+#ifdef CONFIG_LGE_DISABLE_SECOND_SCREEN
+	input_set_abs_params(input, ABS_MT_POSITION_Y, 160,
+			ts->caps.max_y, 0, 0);
+#else
+>>>>>>> 629b7be444e6... Fix disabling of second screen
 	input_set_abs_params(input, ABS_MT_POSITION_Y, 0,
 			ts->caps.max_y, 0, 0);
 	input_set_abs_params(input, ABS_MT_PRESSURE, 0,
